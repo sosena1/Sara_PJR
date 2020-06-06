fb366d0984d93d1cab43e25a421f5bf83213e6d4
Michał Sosna <micsosna@gmail.com>
2020-06-06 02:53:31 +0200

    csv data

--- Projekt_PJR.R
+++ Projekt_PJR.R
@@ -1,5 +1,10 @@
 #Sara Rupacz - projekt PJR
 
-#getwd()
-#setwd("C:/Users/Sosna/Desktop/PJR Sara/")
+getwd()
+setwd("C:/Users/Sosna/Desktop/PJR Sara/Sara_PJR/")
+
+library(fivethirtyeight)
+
+x <- fivethirtyeight::comic_characters
+write.csv(x, file = "./NotProcessedData.csv")
 
